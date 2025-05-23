7223ebeaa8d5cbcea0806219790db527656c7ef1
Thiago Loure <thiagoloure.developer@gmail.com>
2025-05-23 00:07:42 -0300

    DTO

---
+++ src/main/java/com/louredev/listgames/dto/GameMinDto.java
@@ -0,0 +1,29 @@
+package com.louredev.listgames.dto;
+
+import com.louredev.listgames.entities.Game;
+
+public class GameMinDto {
+		
+	private Long id;
+	private String title;
+	private Integer year;
+	private String imgUrl;
+	private String shortDescription;
+	
+	
+	public GameMinDto() {
+		
+	}
+
+
+	public GameMinDto(Game entities) {
+		super();
+		id = entities.getId();
+		this.title = entities.getTitle();
+		this.year = entities.getYear();
+		this.imgUrl = entities.getImgUrl();
+		this.shortDescription = entities.getShortDescription();
+	}
+	
+	
+}
