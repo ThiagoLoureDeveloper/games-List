7cf2af937f86e2cf2e3e9751e86ba20eeed1bfb5
Thiago Loure <thiagoloure.developer@gmail.com>
2025-05-22 23:16:12 -0300

    PostOne

---
+++ src/test/java/com/louredev/listgames/ListGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louredev.listgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
